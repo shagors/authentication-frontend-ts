319747838e88b0cf5d9e2793e94621ff10c6ba88
shagors <shajjadshagor@gmail.com>
2024-03-25 07:24:54 +0600

    make dashboard some design and base url change and test it and make error and success message

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="max-w-7xl mx-auto min-h-screen">
+      <h1 className="bg-blue-700 text-white text-center py-8 text-3xl">
+        Dashboard
+      </h1>
+      <div className="pt-12 flex justify-between items-center">
+        <div className="pl-4">
+          <h4 className="text-xl font-medium">Name: LWG</h4>
+          <h4 className="text-xl font-medium"> Email: LWG@outlook.com</h4>
+          <h4 className="text-xl font-medium">Rights: Admin</h4>
+        </div>
+        <div className="pr-12">
+          <Button className="bg-red-600">Logout</Button>
+        </div>
+      </div>
+      <div className="mt-24 flex items-center justify-center">Form</div>
+    </div>
+  );
 };
 
 export default Dashboard;
--- src/redux/baseApi/baseApi.ts
+++ src/redux/baseApi/baseApi.ts
@@ -4,7 +4,8 @@ export const baseApi = createApi({
   reducerPath: "baseApi",
   tagTypes: ["user"],
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000",
+    // baseUrl: "http://localhost:5000",
+    baseUrl: "https://auth-backend-ebon.vercel.app",
     // credentials: "include",
   }),
   endpoints: () => ({}),
